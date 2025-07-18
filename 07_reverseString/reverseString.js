dfff25b2e59d5b92867b2411f83c5b44804382b2
davidmshayne <63462420+David-Shayne@users.noreply.github.com>
2025-07-18 10:39:45 +0200

    Complete 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function (str) {
+	let strArray = [...str];
+	let reversedStr = [];
 
+	for (let i = str.length - 1; i >= 0; i--) {
+		//Get end character
+		let char = strArray.pop();
+		//Add it to beginning of new array
+		reversedStr.push(char);
+	}
+
+	return reversedStr.join("");
 };
 
 // Do not edit below this line
