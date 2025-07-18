10c53fc00d3627316088becd11d8fe8f630c1d3a
davidmshayne <63462420+David-Shayne@users.noreply.github.com>
2025-07-18 12:11:52 +0200

    Complete 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function (year) {
+	let divisableByFour = year % 4 === 0;
+	let divisableByHundred = year % 100 === 0;
+	let divisableByFourHundred = year % 400 === 0;
 
+	if (divisableByFour && (!divisableByHundred || divisableByFourHundred)) {
+		return true;
+	} else return false;
 };
 
 // Do not edit below this line
