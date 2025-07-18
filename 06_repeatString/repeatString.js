2136dbaae89519238ede34a1f7cef1467a881801
davidmshayne <63462420+David-Shayne@users.noreply.github.com>
2025-07-18 10:04:54 +0200

    Complete 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+	let repeatedStr = "";
 
+	//Negative numbers invalid -> returns an error string
+	if (num < 0) {
+		return "ERROR";
+	}
+
+	for (let i = 1; i <= num; i++) {
+		repeatedStr += str;
+	}
+
+	return repeatedStr;
 };
 
 // Do not edit below this line
