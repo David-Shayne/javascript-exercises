70db72f72b21342fecf37bd4b8c1bfd09245bc08
davidmshayne <63462420+David-Shayne@users.noreply.github.com>
2025-07-18 12:18:47 +0200

    11 completed

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+	(celcius = (fahrenheit - 32) * (5 / 9)), 1;
+	return Math.round(celcius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+	(fahrenheit = celcius / (5 / 9) + 32), 1;
+	return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
