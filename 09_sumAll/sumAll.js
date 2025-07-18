3b2965ba8bd1176194f6b119a8ee8d3fca480b93
davidmshayne <63462420+David-Shayne@users.noreply.github.com>
2025-07-18 11:55:51 +0200

    Complete 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+//Take in two positive inegers
+//Get an array of all integers between them (including them)
+//For loop starting at lower integer then ending at higher integer
+//Each occurance multiplies the previous total by the current integer
 
+const sumAll = function (int1, int2) {
+	let sum = 0;
+
+	//Checks arguments are whole numbers
+	for (arg of [int1, int2]) {
+		if (typeof arg !== "number" || arg < 0 || arg % 1 !== 0) {
+			return "ERROR";
+		}
+	}
+
+	let maxInt = Math.max(int1, int2);
+	let minInt = Math.min(int1, int2);
+
+	for (let i = minInt; i <= maxInt; i++) {
+		sum === 0 ? (sum = i) : (sum += i);
+	}
+
+	return sum;
 };
 
 // Do not edit below this line
